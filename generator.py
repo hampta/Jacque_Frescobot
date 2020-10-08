8a761676c49482758d5c536c668872510d4f47a3
hampta <art.mr.hampta@gmail.com>
2020-10-08 19:26:46 +0300

    fix RGBA

--- generator.py
+++ generator.py
@@ -34,8 +34,8 @@ def auto(self, text1, text2=''):
     src_file = Image.open(f'{self}')
     file_s1 = src_file.size[0] + 50
     file_s2 = src_file.size[1] + 50
-    _src_file = Image.new("RGBA", size=src_file.size)
-    _src_file = Image.alpha_composite(_src_file,src_file)
+    _src_file = Image.new("RGBA", size=src_file.size, color='black')
+    _src_file = Image.alpha_composite(_src_file, src_file)
     lines1 = textwrap.wrap(text1, width=src_file.size[0] // 18)
     lines2 = textwrap.wrap(text2, width=src_file.size[0] // 15)
     base = _src_file.crop((-50, -50, file_s1, src_file.size[1] + 20 + 35 * len(lines1) + 25 * len(lines2)))
@@ -52,8 +52,8 @@ def auto(self, text1, text2=''):
     for line in lines2:
         d.text(((base.width - d.textsize(line, fnt2)[0]) / 2, h2), line, font=fnt2)
         h2 += 25
-    base.save(f'{self}')
-    return f'{self}'
+    base.save(f'{self}.jpg')
+    return f'{self}.jpg'
 
 
 async def dem_gen(self):
