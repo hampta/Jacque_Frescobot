a482ddea32da0de36fc2041dced50f8eb07c2772
hampta <art.mr.hampta@gmail.com>
2020-10-08 18:52:33 +0300

    fix RGBA error

--- generator.py
+++ generator.py
@@ -50,6 +50,7 @@ def auto(self, text1, text2=''):
     for line in lines2:
         d.text(((base.width - d.textsize(line, fnt2)[0]) / 2, h2), line, font=fnt2)
         h2 += 25
+    base.convert("RGB")
     base.save(f'{self}')
     return f'{self}'
 
