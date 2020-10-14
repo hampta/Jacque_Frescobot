0ca622bab8f55c6584872f67021dccea70813090
hampta <art.mr.hampta@gmail.com>
2020-10-15 00:25:06 +0300

    UPDATE!

--- defines.py
+++ defines.py
@@ -4,12 +4,18 @@ from tortoise import Tortoise, fields
 from tortoise.models import Model
 from aiogram import types
 import os
+import time
+from generator import classic, auto
+from wand.display import display
+from wand.image import Image
+
 
 class Validators(PatchedValidators):
     def int(self, text: str, *args):
         valid = text.isdigit()
         if valid:
             return int(text)
+
 
 class Event(Model):
     chat_id = fields.BigIntField(pk=True)
@@ -24,7 +30,11 @@ class Event(Model):
 
 patcher = Patcher(validators=Validators)
 text_pattern = patcher.pattern("/dm <(//)*text>")
-text_pattern2 = patcher.pattern("/dmc <(//)*text>")
+text_pattern2 = patcher.pattern("/liquid <ratio:int>")
+text_pattern3 = patcher.pattern("/swirl <ratio>")
+text_pattern4 = patcher.pattern("/wave <ratio:int> <length:int>")
+text_pattern5 = patcher.pattern("/contrast <ratio:int>")
+text_pattern6 = patcher.pattern("/jpeg <ratio:int>")
 
 
 async def call():
@@ -32,12 +42,23 @@ async def call():
                         modules={"models": ["__main__"]})
 
 
-async def chat(message: types.Message) -> bool:
+async def check(message: types.Message, file_name):
     if message.photo:
+        await message.photo[-1].download(destination=file_name)
         return True
+    if message.reply_to_message and message.reply_to_message.photo:
+        await message.reply_to_message.photo[-1].download(destination=file_name)
+        return True
+    if message.reply_to_message and message.reply_to_message.sticker:
+        if not message.reply_to_message.sticker.is_animated:
+            await message.reply_to_message.sticker.download(destination=file_name)
+            return True
+        else:
+            return False
+    else:
+        return None
 
-
-async def check(message: types.Message) -> str:
+async def check_dem(message: types.Message):
     if message.photo:
         return "message_photo"
     if message.reply_to_message and message.reply_to_message.photo:
@@ -46,51 +67,70 @@ async def check(message: types.Message) -> str:
         if not message.reply_to_message.sticker.is_animated:
             return "sticker"
         else:
-            return 'anim_sticker'
+            return False
     else:
         return None
 
-
-async def textcheck(message: types.Message):
+async def text_check(message: types.Message):
     if text_pattern(message.text) is not None:
-        text = patcher.check(message.text, text_pattern)
+        return patcher.check(message.text, text_pattern)
     elif text_pattern(message.caption) is not None:
-        text = patcher.check(message.caption, text_pattern)
+        return patcher.check(message.caption, text_pattern)
     else:
         return None
-    return text
 
 
-async def textcheck2(message: types.Message):
-    if text_pattern2(message.text) is None:
-        return None
-    text = patcher.check(message.text, text_pattern2)
-    return text
+async def text_check_liquid(message: types.Message):
+    if text_pattern2(message.text) is not None:
+        return patcher.check(message.text, text_pattern2)
+    elif text_pattern2(message.caption) is not None:
+        return patcher.check(message.caption, text_pattern2)
+    else:
+        return {"ratio": 35}
 
 
+async def text_check_swirl(message: types.Message):
+    if text_pattern3(message.text) is not None:
+        return patcher.check(message.text, text_pattern3)
+    elif text_pattern3(message.caption) is not None:
+        return patcher.check(message.caption, text_pattern3)
+    else:
+        return {"ratio": 90}
 
-# async def check(message: types.Message) -> bool:
-#    if message.reply_to_message and message.reply_to_message.photo:
-#        return True
+
+async def text_check_wave(message: types.Message):
+    if text_pattern4(message.text) is not None:
+        return patcher.check(message.text, text_pattern4)
+    elif text_pattern4(message.caption) is not None:
+        return patcher.check(message.caption, text_pattern4)
+    else:
+        return {"ratio": 30, "length": 90}
+
+
+async def text_check_contrast(message: types.Message):
+    if text_pattern5(message.text) is not None:
+        return patcher.check(message.text, text_pattern5)
+    elif text_pattern5(message.caption) is not None:
+        return patcher.check(message.caption, text_pattern5)
+    else:
+        return {"ratio": 100}
 
 
 async def statistics_write(tg_chat_id):
     await call()
     info = await Event.filter(chat_id=tg_chat_id).values()
-    print(info)
     if info == []:
-        await Event.create(chat_id=tg_chat_id,used=0)
+        await Event.create(chat_id=tg_chat_id, used=0)
         used = 0
     else:
         used = info[0]['used']
-    await Event.filter(chat_id=tg_chat_id).update(used=used+1)
+    await Event.filter(chat_id=tg_chat_id).update(used=used + 1)
 
 
 async def statistics_read(chat_id):
     await call()
     info = await Event.filter().values()
     info_chat = await Event.filter(chat_id=chat_id).values()
-    print(info_chat)
     all_used = 0
     for i in info:
         all_used += i['used']
@@ -101,4 +141,94 @@ async def statistics_read(chat_id):
           f"Использовалось в этом чате: " \
           f"{info_chat[0]['used']}"
     return mes
+
+
+async def photo_check(message):
+    file_name = f"photos/{message.chat.id}_{time.time()}.png"
+    text = await text_check(message)
+    attachments = await check_dem(message)
+    if attachments == "message_photo":
+        if text is not None:
+            await message.photo[-1].download(destination=file_name)
+    elif attachments == "reply_photo":
+        if text is not None:
+            await message.reply_to_message.photo[-1].download(destination=file_name)
+    elif attachments == "sticker":
+        if text is not None:
+            await message.reply_to_message.sticker.download(destination=file_name)
+            if text is None:
+                return "А текст где"
+            if len(text["text"]) == 1:
+                return classic(file_name, text["text"][0])
+            elif len(text["text"]) >= 2:
+                return classic(file_name, text["text"][0], text["text"][1])
+    elif attachments == "anim_sticker":
+        return "Анимированный стикер не подходит!"
+    else:
+        return "А где картинка"
+    if text is None:
+        return
+    if len(text["text"]) == 1:
+        return auto(file_name, text["text"][0])
+    elif len(text["text"]) >= 2:
+        return auto(file_name, text["text"][0], text["text"][1])
+
+
+async def liquid_photo(message):
+    file_name = f"photos/{message.chat.id}_{time.time()}.png"
+    text = await text_check_liquid(message)
+    if await check(message, file_name):
+        ratio = text['ratio'] / 100
+        with Image(filename=file_name) as img:
+            with img.clone() as i:
+                size = i.size
+                i.liquid_rescale(int(size[0] * ratio), int(size[1] * ratio))
+                i.resize(size[0], size[1])
+                i.save(filename=file_name)
+                return file_name
+    else:
+        return "Нужна картинка или стикер!"
+
+
+async def swirl_photo(message):
+    file_name = f"photos/{message.chat.id}_{time.time()}.jpg"
+    text = await text_check_swirl(message)
+    if await check(message, file_name):
+        ratio = text['ratio']
+        with Image(filename=file_name) as img:
+            with img.clone() as i:
+                i.swirl(degree=int(ratio))
+                i.save(filename=file_name)
+                return file_name
+    else:
+        return "Нужна картинка или стикер!"
+
+
+async def wave_photo(message):
+    file_name = f"photos/{message.chat.id}_{time.time()}.jpg"
+    text = await text_check_wave(message)
+    if await check(message, file_name):
+        ratio = text['ratio']
+        length = text['length']
+        with Image(filename=file_name) as img:
+            with img.clone() as i:
+                i.wave(ratio, length)
+                i.save(filename=file_name)
+                return file_name
+    else:
+        return "Нужна картинка или стикер!"
+
+
+async def contrast_photo(message):
+    file_name = f"photos/{message.chat.id}_{time.time()}.jpg"
+    text = await text_check_contrast(message)
+    if await check(message, file_name):
+        ratio = text['ratio']
+        with Image(filename=file_name) as img:
+            with img.clone() as i:
+                i.brightness_contrast(contrast=ratio)
+                i.save(filename=file_name)
+                return file_name
+    else:
+        return "Нужна картинка или стикер!"
 
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import logging
 import os
 import time
+import sys
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Command
 from aiogram.types import InputFile, ContentType
 from defines import *
+from imagemagick import *
 from generator import auto, classic
 
-bot = Bot(str(os.getenv("TOKEN")))
+# bot = Bot(str(os.getenv("TOKEN")))
+
+bot = Bot("1337136747:AAGJU8T-EUXZpR_xoUl7hTcLfvV8IXKBhaU")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+sys.setrecursionlimit(2024)
+skip_list = ["Анимированный стикер не подходит!", "А текст где", "А где картинка", "Допустимый коэфициент от 1 до 200",
+             "Недопустимый коэфициент"]
 
 help_message = "Список команд для бота:\n" \
                "Генерация демотиватора из приклеплённой картинки\n" \
@@ -18,49 +26,18 @@ help_message = "Список команд для бота:\n" \
                "Канал с демотиваторами из бота: https://t.me/Jacque_Fresco_memes"
 
 
-
-
-@dp.message_handler(commands=["dm"])
+@dp.message_handler(Command(['dm'], ignore_caption=False), content_types=[ContentType.TEXT, ContentType.PHOTO])
 async def generate_modern(message: types.Message):
     try:
-        file_name = f"photos/{message.chat.id}_{time.time()}.png"
-        textch = await textcheck(message)
-        attachments = await check(message)
-        if attachments == "message_photo":
-            if textch is not None:
-                await message.photo[-1].download(destination=file_name)
-        elif attachments == "reply_photo":
-            if textch is not None:
-                await message.reply_to_message.photo[-1].download(destination=file_name)
-        elif attachments == "sticker":
-            if textch is not None:
-                await message.reply_to_message.sticker.download(destination=file_name)
-                if textch is None:
-                    return
-                if len(textch["text"]) == 1:
-                    media = InputFile(classic(file_name, textch["text"][0]))
-                elif len(textch["text"]) >= 2:
-                    media = InputFile(classic(file_name, textch["text"][0], textch["text"][1]))
-                await bot.send_photo(chat_id=message.chat.id, photo=media, reply_to_message_id=message.message_id)
-                os.remove(file_name)
-                await statistics_write(message.chat.id)
-                return
-        elif attachments == "anim_sticker":
-            await bot.send_message(chat_id=message.chat.id, text=f"Анимированный стикер не подходит!")
+        photo = await photo_check(message)
+        if photo in skip_list:
+            await bot.send_message(chat_id=message.chat.id, text=photo)
         else:
-            await bot.send_message(chat_id=message.chat.id, text=f"А где картинка")
-            return
-        if textch is None:
-            return
-        if len(textch["text"]) == 1:
-            media = InputFile(auto(file_name, textch["text"][0]))
-        elif len(textch["text"]) >= 2:
-            media = InputFile(auto(file_name, textch["text"][0], textch["text"][1]))
-        await bot.send_photo(chat_id=message.chat.id, photo=media, reply_to_message_id=message.message_id)
-        os.remove(file_name)
+            await bot.send_photo(chat_id=message.chat.id, photo=InputFile(photo))
+            os.remove(photo)
         await statistics_write(message.chat.id)
     except Exception as e:
-        await bot.send_message(chat_id=message.chat.id, text=f"ERROR код ошибки \n{e}")
+        await bot.send_message(chat_id=message.chat.id, text=f"Error код ошибки \n{e}")
 
 
 @dp.message_handler(commands=["message"])
@@ -78,6 +55,53 @@ async def text_in_handler(message: types.Message):
     await bot.send_message(chat_id=message.chat.id, text=help_message)
 
 
+@dp.message_handler(Command(["cas"], ignore_caption=False), content_types=[ContentType.TEXT, ContentType.PHOTO])
+async def liquid(message: types.Message):
+    photo = await liquid_photo(message)
+    print(photo)
+    if photo in skip_list:
+        await bot.send_message(chat_id=message.chat.id, text=photo)
+    else:
+        await bot.send_photo(chat_id=message.chat.id, photo=InputFile(photo))
+        os.remove(photo)
+    await statistics_write(message.chat.id)
+
+@dp.message_handler(Command(["sw"]),
+                    content_types=[ContentType.TEXT, ContentType.PHOTO])
+async def swirl(message: types.Message):
+    photo = await swirl_photo(message)
+    print(photo)
+    if photo in skip_list:
+        await bot.send_message(chat_id=message.chat.id, text=photo)
+    else:
+        await bot.send_photo(chat_id=message.chat.id, photo=InputFile(photo))
+        os.remove(photo)
+    await statistics_write(message.chat.id)
+
+
+@dp.message_handler(Command(["wave"]),
+                    content_types=[ContentType.TEXT, ContentType.PHOTO])
+async def swirl(message: types.Message):
+    photo = await wave_photo(message)
+    print(photo)
+    if photo in skip_list:
+        await bot.send_message(chat_id=message.chat.id, text=photo)
+    else:
+        await bot.send_photo(chat_id=message.chat.id, photo=InputFile(photo))
+        os.remove(photo)
+    await statistics_write(message.chat.id)
+
+@dp.message_handler(Command(["contast"]),
+                    content_types=[ContentType.TEXT, ContentType.PHOTO])
+async def swirl(message: types.Message):
+    photo = await contrast_photo(message)
+    print(photo)
+    if photo in skip_list:
+        await bot.send_message(chat_id=message.chat.id, text=photo)
+    else:
+        await bot.send_photo(chat_id=message.chat.id, photo=InputFile(photo))
+        os.remove(photo)
+    await statistics_write(message.chat.id)
+
 if __name__ == '__main__':
-    print("Бот запущен!")
     executor.start_polling(dp, skip_updates=True)
